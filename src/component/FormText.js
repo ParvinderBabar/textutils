3aa078d3e7268502ae3526c33185ed46b0ff3763
parvinderBabar <er.kaurparvinder@gmail.com>
2024-01-20 23:35:28 -0500

    FORM READY

--- src/component/FormText.js
+++ src/component/FormText.js
@@ -9,6 +9,7 @@ export default function FormText(props) {
             <h1> {props.heading}</h1>
             <textarea class="form-control" id="Textarea" rows="8" />
           </div>
+          <button className="btn btn-primary">ConvertUppercase</button>
         </form>
       </div>
     </>
