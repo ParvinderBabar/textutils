0e982580278c3f78779d0d55966a1355c25ebe9b
parvinderBabar <er.kaurparvinder@gmail.com>
2024-01-21 18:23:58 -0500

    chngesinForm

--- src/component/FormText.js
+++ src/component/FormText.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function FormText(props) {
+  const [text, setText] = useState[""];
+  const uppercase = () => {
+    let textupdated = text.toUppercase();
+    setText(textupdated);
+  };
+  const handleText = (event) => {
+    setText(event.target.value);
+  };
   return (
     <>
       <div>
         <form>
           <div className="mb-3">
             <h1> {props.heading}</h1>
-            <textarea class="form-control" id="Textarea" rows="8" />
+            <textarea
+              class="form-control"
+              id="Textarea"
+              value={text}
+              onChange={handleText}
+              rows="8"
+            />
           </div>
-          <button className="btn btn-primary">ConvertUppercase</button>
+          <button className="btn btn-primary" onClick={uppercase}>
+            ConvertUppercase
+          </button>
         </form>
       </div>
     </>
